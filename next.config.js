cfd98da8fe2d134920e45bc2bad50878af3fd070
fawazabdillah499-stack <fawazabdillah499@gmail.com>
2026-05-14 17:47:44 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 module.exports = nextConfig
